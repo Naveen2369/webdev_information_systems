060d5723e01dec95126bd17f9b47ce46271dafb1
Naveen2369 <137410722+Naveen2369@users.noreply.github.com>
2024-03-18 22:55:27 +0000

    added colors and images

--- backend/models/ProductModel.js
+++ backend/models/ProductModel.js
@@ -20,6 +20,31 @@ const productSchema = mongoose.Schema(
     category: {
       required: true,
       type: String,
-    }
+    },
+    colors: {
+      type: [Map],
+    },
+    sizes: {
+      type: [Map],
+    },
+    image1: {
+      required: true,
+      type: String,
+    },
+    image2: {
+      required: true,
+      type: String,
+    },
+    image3: {
+      required: true,
+      type: String,
+    },
+    description: {
+      required: true,
+      type: String,
+    },
+    reviews: [{ type: mongoose.Types.ObjectId, ref: "review" }],
+  },
+  { timestamps: true }
 );
 module.exports = mongoose.model("product", productSchema);
